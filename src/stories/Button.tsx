efa697560805c4cdc67f1290ecf2cd2a35c62317
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-10 17:08:12 +0300

    fix emotion css problem

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,39 +1,74 @@
-import "./button.css"
-
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
+type ButtonProps = {
   primary?: boolean
-  /** What background color to use */
-  backgroundColor?: string
-  /** How large should the button be? */
-  size?: "small" | "medium" | "large"
-  /** Button contents */
   label: string
-  /** Optional click handler */
+  size?: "small" | "large"
+  backgroundColor?: string
   onClick?: () => void
 }
 
-/** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
+const Button = ({
+  primary = true,
   label,
-  ...props
+  size = "small",
+  onClick,
 }: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary"
+  const buttonStyles = [
+    buttonBaseStyle,
+    primary ? primaryButtonStyle : secondaryButtonStyle,
+    size === "small"
+      ? smallButtonStyle
+      : size === "large"
+      ? largeButtonStyle
+      : null,
+  ]
+
   return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button css={buttonStyles} onClick={onClick}>
       {label}
     </button>
   )
 }
+
+export default Button
+
+// Button.styles.ts
+import { css } from "@emotion/react"
+
+export const buttonBaseStyle = css`
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 10px 20px;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export const primaryButtonStyle = css`
+  background-color: #007bff;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`
+
+export const secondaryButtonStyle = css`
+  background-color: #ccc;
+
+  &:hover {
+    background-color: #999;
+  }
+`
+
+export const smallButtonStyle = css`
+  padding: 8px 16px;
+  font-size: 14px;
+`
+
+export const largeButtonStyle = css`
+  padding: 16px 32px;
+  font-size: 18px;
+`
