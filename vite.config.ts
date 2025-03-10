7dcff9e953cd98bfdc51f4edf68889a3c3c6ab6d
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-10 14:59:56 +0300

    setting up vite and storybook

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { dirname, resolve, relative, extname } from "node:path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import react from "@vitejs/plugin-react"
+import { libInjectCss } from "vite-plugin-lib-inject-css"
+import { glob } from "glob"
+import { fileURLToPath } from "node:url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts(), libInjectCss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "nevermined-ui",
+      fileName: "nevermined-ui",
+      formats: ["es"],
+    },
+
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("src/**/*.{ts,tsx}")
+          .map((file) => [
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+})
