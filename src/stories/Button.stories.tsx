a693adf9edb607c909a0c7301b3a53fb478a560c
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-11 16:07:09 +0300

    added input component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,62 +1,62 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from "@storybook/react"
+import { fn } from "@storybook/test"
 
-import { Button } from '../ui/Button';
+import { Button } from "../ui/Button"
 
 const meta = {
-  title: 'Example/Button',
+  title: "Components/Button",
   component: Button,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
     // backgroundColor: { control: 'color' },
   },
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Button>
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    children: 'Button',
+    children: "Button",
   },
-};
+}
 
 export const Secondary: Story = {
   args: {
-    variant: 'secondary',
-    children: 'Secondary',
+    variant: "secondary",
+    children: "Secondary",
   },
-};
+}
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    children: 'Large',
+    size: "large",
+    children: "Large",
   },
-};
+}
 
 export const Medium: Story = {
   args: {
-    size: 'medium',
-    children: 'Medium',
+    size: "medium",
+    children: "Medium",
   },
-};
+}
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    children: 'Small',
+    size: "small",
+    children: "Small",
   },
-};
+}
 
 export const Ghost: Story = {
   args: {
-    variant: 'ghost',
-    children: 'Ghost',
+    variant: "ghost",
+    children: "Ghost",
   },
-};
+}
---
+++ src/ui/Input.tsx
@@ -0,0 +1,262 @@
+import { inputDisabled, purple, purple20, purple5, red } from "@/styles/colors"
+import { errorMessageStyle } from "@/styles/styles"
+
+import { css, type Interpolation, type Theme } from "@emotion/react"
+import React, {
+  HTMLProps,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
+
+import Row from "./Row"
+import Column from "./Column"
+import { PlacesType, Tooltip } from "./tooltip/tooltip"
+
+export interface InputProps extends HTMLProps<HTMLInputElement> {
+  info?: string | null
+  infoPosition?: PlacesType
+  name?: string
+  cssStyle?: Interpolation<Theme>
+  inputWrapperCssStyle?: Interpolation<Theme>
+  inputCssStyle?: Interpolation<Theme>
+  required?: boolean
+  isInvalid?: boolean
+  errorMessage?: string
+  innerLabel?: ReactNode | null
+  innerSecondaryLabel?: ReactNode | null
+  icon?: ReactNode
+  testid?: string
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({
+    name,
+    cssStyle,
+    inputWrapperCssStyle,
+    inputCssStyle,
+    innerLabel,
+    innerSecondaryLabel,
+    errorMessage,
+    disabled,
+    icon,
+    info,
+    infoPosition,
+    required,
+    value,
+    isInvalid,
+    testid,
+    ...rest
+  }: InputProps) => {
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    const [active, setActive] = useState(false)
+    const [raisedLabel, setRaisedLabel] = useState(!!(active || value))
+
+    useEffect(() => {
+      setRaisedLabel(!!(active || value))
+    }, [active, value])
+
+    const onCreditsChanged = useCallback(
+      (e: React.FormEvent<HTMLInputElement>) => {
+        if (
+          e.currentTarget.type === "number" &&
+          e.currentTarget.min &&
+          e.currentTarget.max
+        ) {
+          e.currentTarget.value = Math.max(
+            Number(e.currentTarget.min),
+            Math.min(Number(e.currentTarget.max), Number(e.currentTarget.value))
+          ).toString()
+        }
+        rest.onChange?.(e)
+      },
+      [rest.onChange]
+    )
+
+    return innerLabel ? (
+      <Column cssStyle={[inputRowContainerStyle, cssStyle]}>
+        <Row>
+          <Row
+            css={[
+              baseStyle,
+              active && inputWithInnerLabelActiveStyle(!!errorMessage),
+              disabled && disabledStyle,
+              (errorMessage || isInvalid) && inputInvalidStyle,
+              !!icon && inputWithIconStyle,
+              inputWrapperCssStyle,
+            ]}
+          >
+            {innerLabel && (
+              <label
+                css={[
+                  innerLabelStyle,
+                  raisedLabel && raisedInnerLabelStyle,
+                  disabled && { color: "rgba(255, 255, 255, 0.3)" },
+                ]}
+                htmlFor={name}
+              >
+                {innerLabel}
+                {innerSecondaryLabel && (
+                  <span css={requiredLabelStyle}>{innerSecondaryLabel}</span>
+                )}
+                {required && <>*</>}
+              </label>
+            )}
+            <input
+              name={name}
+              css={[
+                inputWithInnerLabelStyle,
+                disabled && disabledStyle,
+                inputCssStyle,
+              ]}
+              disabled={disabled}
+              value={value}
+              {...rest}
+              onChange={onCreditsChanged}
+              placeholder=""
+              onFocus={(e) => {
+                rest.onFocus?.(e)
+                setActive(true)
+                setRaisedLabel(true)
+              }}
+              onBlur={(e) => {
+                rest.onBlur?.(e)
+                setActive(false)
+                setRaisedLabel(!!inputRef.current?.value?.length)
+              }}
+            />
+            {icon}
+          </Row>
+          {info && infoPosition && (
+            <Tooltip
+              cssStyle={css({
+                marginLeft: 18,
+              })}
+              place={infoPosition}
+              showIcon
+            >
+              {info}
+            </Tooltip>
+          )}
+        </Row>
+        {errorMessage && <span css={errorMessageStyle}>{errorMessage}</span>}
+      </Column>
+    ) : (
+      <>
+        <Row css={[{ gap: 10 }, cssStyle]}>
+          <Row css={[cssStyle, !!icon && inputWithIconStyle]}>
+            <input
+              name={name}
+              css={[
+                baseStyle,
+                (errorMessage || isInvalid) && inputInvalidStyle,
+                disabled && disabledStyle,
+                inputCssStyle,
+              ]}
+              disabled={disabled}
+              value={value}
+              {...rest}
+              onChange={onCreditsChanged}
+            />
+            {icon}
+          </Row>
+          {info && infoPosition && (
+            <Tooltip
+              cssStyle={css({
+                marginLeft: 5,
+              })}
+              place={infoPosition}
+              showIcon
+            >
+              {info}
+            </Tooltip>
+          )}
+        </Row>
+        {errorMessage && <span css={errorMessageStyle}>{errorMessage}</span>}
+      </>
+    )
+  }
+)
+
+const commonBorderStyle = {
+  borderRadius: 4,
+}
+
+const baseStyle = css({
+  ...commonBorderStyle,
+  border: `1px solid ${purple20}`,
+  width: "100%",
+  height: 56,
+  position: "relative",
+  backgroundColor: "transparent",
+})
+
+const inputInvalidStyle = css({
+  boxShadow: `0 0 0 1px  #FF5F5F`,
+  border: `1px solid transparent`,
+})
+
+export const inputWithInnerLabelActiveStyle = (isInvalid = false) =>
+  css({
+    boxShadow: `0 0 0 2px ${isInvalid ? red : purple}`,
+    backgroundColor: purple5,
+  })
+
+const innerLabelStyle = css({
+  fontSize: "14px",
+  fontWeight: 400,
+  color: "#00003866",
+  position: "absolute",
+  top: "28px",
+  left: "16px",
+  transform: "translateY(-50%)",
+  transition: "transform 150ms ease-out, font-size 150ms ease-out",
+  pointerEvents: "none",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  paddingRight: "32px",
+  width: "100%",
+  display: "flex",
+  gap: "0.25rem",
+})
+
+const raisedInnerLabelStyle = css({
+  top: "25px",
+  transform: "translateY(-150%)",
+  fontSize: "10px",
+})
+
+const inputWithInnerLabelStyle = css({
+  ...commonBorderStyle,
+  outline: "none",
+  border: "none",
+  display: "block",
+  background: "transparent",
+  padding: "8px 16px 0",
+  width: "100%",
+  height: "100%",
+  fontSize: 15,
+})
+
+const disabledStyle = css({
+  color: "rgba(0, 0, 0, 0.3)",
+  backgroundColor: inputDisabled,
+})
+
+const inputWithIconStyle = css({
+  position: "relative",
+})
+
+const inputRowContainerStyle = css({
+  alignSelf: "stretch",
+  width: "100%",
+  gap: 10,
+  alignItems: "flex-start",
+})
+
+const requiredLabelStyle = css({
+  color: "rgba(0, 0, 0, 0.5)",
+})
