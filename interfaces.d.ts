7d6d7ad8465d9fe83d089190ed0d4dbcc503f890
Iurii ᚾ <iurii.tankievych@outlook.com>
2025-03-11 08:56:51 +0000

    add: styles and image

---
+++ interfaces.d.ts
@@ -0,0 +1,32 @@
+// This file exists because imports with ?url resource query wouldn't work when defined inside the global typing file
+declare module '*.svg' {
+  import type { ReactElement, SVGProps } from 'react';
+
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
+
+declare module '*.svg?react' {
+  import type { ReactElement, SVGProps } from 'react';
+
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
+
+declare module '*.svg?url' {
+  const value: string;
+  export default value;
+}
+
+// For PNG files
+declare module '*.png' {
+  import type { ReactElement, SVGProps } from 'react';
+
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
+
+declare module '*.png?url' {
+  const value: string;
+  export default value;
+}
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,74 +1,202 @@
-type ButtonProps = {
-  primary?: boolean
-  label: string
-  size?: "small" | "large"
-  backgroundColor?: string
-  onClick?: () => void
+import ExternalLinkIcon from '@/assets/icons/external-link.svg?react';
+import { black, buttonLabel, purple, white } from '@/styles/colors';
+import {
+  buttonLabelFontSize,
+  buttonLabelFontWeight,
+  buttonLabelLineHeight,
+  buttonLargeFontSize,
+  buttonLargeFontWeight,
+  buttonSmallFontSize,
+  buttonSmallFontWeight,
+} from '@/styles/styles';
+import { Interpolation, Theme, css } from '@emotion/react';
+import React, { ReactNode } from 'react';
+
+interface ButtonCommonProps {
+  variant?: 'primary' | 'secondary' | 'ghost';
+  size?: 'large' | 'medium' | 'small' | 'label' | 'label-small';
+  inactive?: boolean;
+  disabled?: boolean;
+  css?: Interpolation<Theme>;
+  cssStyle?: Interpolation<Theme>;
+  testid?: string;
 }
 
-const Button = ({
-  primary = true,
-  label,
-  size = "small",
-  onClick,
-}: ButtonProps) => {
-  const buttonStyles = [
-    buttonBaseStyle,
-    primary ? primaryButtonStyle : secondaryButtonStyle,
-    size === "small"
-      ? smallButtonStyle
-      : size === "large"
-      ? largeButtonStyle
-      : null,
-  ]
+interface HTMLButtonProps extends React.ButtonHTMLAttributes<any> {
+  children?: ReactNode;
+}
 
-  return (
-    <button css={buttonStyles} onClick={onClick}>
-      {label}
+export type ButtonProps = ButtonCommonProps & HTMLButtonProps;
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      onClick,
+      type = 'button',
+      cssStyle,
+      variant = 'primary',
+      size = 'medium',
+      inactive = false,
+      ...rest
+    },
+    ref
+  ) => (
+    <button
+      ref={ref}
+      type={type}
+      css={[
+        buttonStyle,
+        // icon !== undefined && buttonWithIconStyle,
+        variant === 'primary' && primaryStyle,
+        variant === 'secondary' && secondaryStyle,
+        size === 'large' && largeStyle,
+        size === 'medium' && mediumStyle,
+        size === 'small' && smallStyle,
+        size === 'label' && labelStyle,
+        size === 'label-small' && labelSmallStyle,
+        inactive && labelInactiveStyle,
+
+        cssStyle,
+      ]}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
     </button>
   )
+);
+
+interface HTMLAnchorProps extends React.AnchorHTMLAttributes<unknown> {
+  children?: ReactNode;
 }
 
-export default Button
+type ButtonLinkProps = ButtonCommonProps &
+  HTMLAnchorProps & {
+    as?: React.ElementType;
+    isExternal?: boolean;
+    withoutLanguagePrefix?: boolean;
+  } & Record<string, unknown>;
 
-// Button.styles.ts
-import { css } from "@emotion/react"
+export const ButtonLink = ({
+  as,
+  children,
+  cssStyle,
+  variant = 'primary',
+  size = 'medium',
+  inactive = false,
+  isExternal,
+  ...rest
+}: ButtonLinkProps) => {
+  const Link = as || 'a';
 
-export const buttonBaseStyle = css`
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  padding: 10px 20px;
+  return (
+    <Link
+      css={[
+        buttonStyle,
+        size === 'large' && largeStyle,
+        size === 'medium' && mediumStyle,
+        size === 'small' && smallStyle,
+        size === 'label' && labelStyle,
+        size === 'label-small' && labelSmallStyle,
+        inactive && labelInactiveStyle,
+        variant === 'primary' && primaryStyle,
+        variant === 'secondary' && secondaryStyle,
+        cssStyle,
+      ]}
+      {...rest}
+    >
+      {children}
+      {isExternal && <ExternalLinkIcon css={{ width: 13, height: 13 }} />}
+    </Link>
+  );
+};
 
-  &:focus {
-    outline: none;
-  }
-`
+const primaryStyle = css({
+  color: white,
+  backgroundColor: purple,
+  '&:hover': {
+    backgroundColor: 'rgba(118, 62, 255, 0.6)',
+  },
+});
 
-export const primaryButtonStyle = css`
-  background-color: #007bff;
+const secondaryStyle = css({
+  backgroundColor: 'transparent',
+  border: `1px solid ${purple}`,
+  color: purple,
 
-  &:hover {
-    background-color: #0056b3;
-  }
-`
+  'svg path': {
+    fill: purple,
+  },
 
-export const secondaryButtonStyle = css`
-  background-color: #ccc;
+  '&:hover svg path': {
+    fill: white,
+  },
 
-  &:hover {
-    background-color: #999;
-  }
-`
+  '&:hover': {
+    backgroundColor: 'rgba(118, 62, 255, 0.6)',
+    color: white,
+  },
+});
 
-export const smallButtonStyle = css`
-  padding: 8px 16px;
-  font-size: 14px;
-`
+const largeStyle = css({
+  height: '48px',
+  padding: '16px 28px',
+  fontSize: buttonLargeFontSize,
+  fontWeight: buttonLargeFontWeight,
+});
 
-export const largeButtonStyle = css`
-  padding: 16px 32px;
-  font-size: 18px;
-`
+const mediumStyle = css({
+  height: '42px',
+  padding: '16px 28px',
+  fontSize: buttonLargeFontSize,
+  fontWeight: buttonLargeFontWeight,
+});
+
+const smallStyle = css({
+  height: '36px',
+  padding: '16px 24px',
+  fontSize: buttonSmallFontSize,
+  fontWeight: buttonSmallFontWeight,
+});
+
+const buttonStyle = css({
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: 'none',
+  borderRadius: 56,
+  gap: 10,
+  cursor: 'pointer',
+  transition: 'background-color 0.2s ease',
+  '&:disabled': {
+    cursor: 'not-allowed',
+    opacity: 0.2,
+  },
+});
+
+const labelInactiveStyle = css({
+  opacity: 0.5,
+});
+
+const labelStyle = css({
+  height: '24px',
+  fontSize: buttonLabelFontSize,
+  fontWeight: buttonLabelFontWeight,
+  lineHeight: buttonLabelLineHeight,
+  backgroundColor: buttonLabel,
+  color: black,
+  borderRadius: '40px',
+  padding: '0 15px',
+});
+
+const labelSmallStyle = css({
+  height: '17px',
+  fontSize: buttonLabelFontSize,
+  fontWeight: buttonLabelFontWeight,
+  lineHeight: buttonLabelLineHeight,
+  backgroundColor: buttonLabel,
+  color: black,
+  borderRadius: '40px',
+  padding: '0 15px',
+});
