a1ed13d564d8df3b1abf690d77da834968f3a6f0
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-13 14:40:37 +0300

    added switch comp

---
+++ src/ui/Switch.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+import * as SwitchPrimitive from "@radix-ui/react-switch"
+import { css } from "@emotion/react"
+
+const switchRootStyle = css`
+  all: unset;
+  width: 42px;
+  height: 24px;
+  background-color: #e5e7eb;
+  border-radius: 9999px;
+  position: relative;
+  transition: background-color 0.2s;
+  cursor: pointer;
+
+  &[data-state="checked"] {
+    background-color: #2563eb;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`
+
+const thumbStyle = css`
+  display: block;
+  width: 20px;
+  height: 20px;
+  background-color: #ffffff;
+  border-radius: 50%;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  transition: transform 0.2s;
+  will-change: transform;
+
+  [data-state="checked"] & {
+    transform: translateX(18px);
+  }
+`
+
+function Switch(
+  props: React.ComponentPropsWithoutRef<typeof SwitchPrimitive.Root>
+) {
+  return (
+    <SwitchPrimitive.Root data-slot="switch" css={switchRootStyle} {...props}>
+      <SwitchPrimitive.Thumb data-slot="switch-thumb" css={thumbStyle} />
+    </SwitchPrimitive.Root>
+  )
+}
+
+export { Switch }
