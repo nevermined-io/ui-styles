423eaae83f5b84b3f98c18a52b01246047059d2b
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-13 14:40:31 +0300

    added radio-group story

---
+++ src/stories/RadioGroup.stories.tsx
@@ -0,0 +1,34 @@
+import { Label } from "@/ui/Label"
+import { RadioGroup, RadioGroupItem } from "@/ui/RadioGroup"
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof RadioGroup> = {
+  title: "Components/RadioGroup",
+  component: RadioGroup,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof RadioGroup>
+
+export const Default: Story = {
+  render: () => (
+    <RadioGroup defaultValue="comfortable">
+      <div css={{ display: "flex", alignItems: "center", gap: 10 }}>
+        <RadioGroupItem value="default" id="r1" />
+        <Label htmlFor="r1">default</Label>
+      </div>
+      <div css={{ display: "flex", alignItems: "center", gap: 10 }}>
+        <RadioGroupItem value="comfortable" id="r2" />
+        <Label htmlFor="r2">comfortable</Label>
+      </div>
+      <div css={{ display: "flex", alignItems: "center", gap: 10 }}>
+        <RadioGroupItem value="compact" id="r3" />
+        <Label htmlFor="r3">compact</Label>
+      </div>
+    </RadioGroup>
+  ),
+}
