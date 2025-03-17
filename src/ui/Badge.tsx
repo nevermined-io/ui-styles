b885d217c196da6ae7faddf93d152d87260826c8
Iurii ᚾ <iurii.tankievych@outlook.com>
2025-03-17 14:41:18 +0000

    fix: build

--- src/ui/Badge.tsx
+++ src/ui/Badge.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { css, SerializedStyles } from "@emotion/react"
-import { purple } from "@/styles/colors"
 
 const badgeStyles = () => css`
   display: inline-flex;
