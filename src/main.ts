1a0c2aa84a19bb59c04164dd8af2c0e8106e7186
hcsoylu <hcsoylu.dev@gmail.com>
2025-03-18 16:44:56 +0300

    small fix

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,38 @@
-export { Tooltip } from '@/ui/tooltip/Tooltip';
-export { Alert } from '@/ui/Alert';
-export { AlertDialog } from '@/ui/AlertDialog';
-export { Badge } from '@/ui/Badge';
-export { Button, ButtonLink } from '@/ui/Button';
-export { Card } from '@/ui/Card';
+export { Tooltip } from "@/ui/tooltip/Tooltip"
+export { Alert } from "@/ui/Alert"
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "@/ui/AlertDialog"
+export { Badge } from "@/ui/Badge"
+export { Button, ButtonLink } from "@/ui/Button"
+export { Card } from "@/ui/Card"
 export {
   Carousel,
   CarouselPagination,
   CarouselProvider,
   CarouselContext,
   useCarousel,
-} from '@/ui/Carousel';
-export { Checkbox } from '@/ui/Checkbox';
-export { default as Column } from '@/ui/Column';
-export { Dialog } from '@/ui/Dialog';
-export { Dropdown } from '@/ui/Dropdown';
-export { Input } from '@/ui/Input';
-export { Label } from '@/ui/Label';
-export { RadioGroup } from '@/ui/RadioGroup';
-export { default as Row } from '@/ui/Row';
-export { Select } from '@/ui/Select';
-export { Switch } from '@/ui/Switch';
-export { Table } from '@/ui/Table';
-export { Tabs } from '@/ui/Tabs';
-export { TextArea } from '@/ui/TextArea';
+} from "@/ui/Carousel"
+export { Checkbox } from "@/ui/Checkbox"
+export { default as Column } from "@/ui/Column"
+export { Dialog } from "@/ui/Dialog"
+export { Dropdown } from "@/ui/Dropdown"
+export { Input } from "@/ui/Input"
+export { Label } from "@/ui/Label"
+export { RadioGroup } from "@/ui/RadioGroup"
+export { default as Row } from "@/ui/Row"
+export { Select } from "@/ui/Select"
+export { Switch } from "@/ui/Switch"
+export { Table } from "@/ui/Table"
+export { Tabs } from "@/ui/Tabs"
+export { TextArea } from "@/ui/TextArea"
