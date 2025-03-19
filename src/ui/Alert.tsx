ebcd97470da73d88f1b520e1c7b37e1ae29b8f22
Iurii ᚾ <iurii.tankievych@outlook.com>
2025-03-19 10:54:27 +0000

    fix: Alert

--- src/ui/Alert.tsx
+++ src/ui/Alert.tsx
@@ -1,41 +1,39 @@
-/** @jsxImportSource @emotion/react */
-import * as React from "react"
-import { css, SerializedStyles } from "@emotion/react"
-import { CircleCheck, TriangleAlert } from "lucide-react"
-
-const successBackground = "#d4edda" // Açık yeşil
-const errorBackground = "#f8d7da" // Açık kırmızı
+import * as React from 'react';
+import { css, Interpolation, Theme } from '@emotion/react';
+import { CircleCheck, TriangleAlert } from 'lucide-react';
 
 const alertBaseStyles = css({
-  padding: "12px 16px",
-  borderRadius: "4px",
-  display: "flex",
-  alignItems: "center",
-  gap: "12px",
-  fontSize: "14px",
-})
+  padding: '12px 16px',
+  borderRadius: '4px',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '12px',
+  fontSize: '14px',
+});
 
 const successStyles = css`
-  background-color: ${successBackground};
+  background-color: #d4edda;
   color: #155724;
   border: 1px solid #c3e6cb;
-`
+`;
 
 const errorStyles = css`
-  background-color: ${errorBackground};
+  background-color: #f8d7da;
   color: #721c24;
   border: 1px solid #f5c6cb;
-`
+`;
 
 function Alert({
-  variant = "success",
+  variant = 'success',
   message,
   cssStyle,
+  title,
   ...props
-}: React.ComponentProps<"div"> & {
-  message: string
-  variant?: "success" | "error"
-  cssStyle?: SerializedStyles
+}: React.ComponentProps<'div'> & {
+  message: string;
+  variant?: 'success' | 'error';
+  cssStyle?: Interpolation<Theme>;
+  title?: string | React.ReactNode;
 }) {
   return (
     <div
@@ -43,20 +41,23 @@ function Alert({
       css={[
         alertBaseStyles,
         cssStyle,
-        variant === "success" ? successStyles : errorStyles,
+        variant === 'success' ? successStyles : errorStyles,
       ]}
       {...props}
     >
-      <div css={{ display: "flex", alignItems: "center", gap: "6px" }}>
-        <div>{variant === "success" ? <CircleCheck /> : <TriangleAlert />}</div>
-        <div css={{ fontWeight: "600" }}>
-          {variant === "success" ? <span>Success!</span> : <span>Error!</span>}
+      <div css={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
+        <div>{variant === 'success' ? <CircleCheck /> : <TriangleAlert />}</div>
+        <div css={{ fontWeight: '600' }}>
+          {variant === 'success' ? (
+            <span>{title ? title : 'Success!'}</span>
+          ) : (
+            <span>{title ? title : 'Error!'}</span>
+          )}
         </div>
       </div>
-
       <div>{message}</div>
     </div>
-  )
+  );
 }
 
-export { Alert }
+export { Alert };
